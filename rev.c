81519fb0883fcd0ccc326ff0eaff444dfbc352ab
Cloud User <ec2-user@ip-172-31-27-187.us-east-2.compute.internal>
2021-09-17 07:04:20 +0000

    addede

--- rev.c
+++ rev.c
@@ -1,25 +1,13 @@
 #include<stdio.h>
-int findReverse(int n)
+int rev()
 {
-   int sum=0;
+   int n,sum=0;
+ scanf("%d", &n);
    while (n!=0)
    {
      sum = sum*10 + n%10;
      n /= 10;
    }
-   return sum;
+    return sum;
 }
 
-int main()
-{
-   int number, reverse;
-
-   printf("Enter a positive interger: ");
-   scanf("%d", &number);
-
-   reverse = findReverse(number);
-
-   printf("The reverse of %d is: %d", number, reverse);
-
-   return 0;
-}
